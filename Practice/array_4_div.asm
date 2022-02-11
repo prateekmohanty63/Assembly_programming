5ac4e3d07d74267f75d3bb44c1c084fc4e4fa44a
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-02-11 10:24:16 +0530

    Added array even sum

--- Practice/array_4_div.asm
+++ Practice/array_4_div.asm
@@ -1,39 +1,41 @@
 DATA SEGMENT
-    ORG 2000h
-    ARR1 DW 01H,02H,04H,04H,08H,012H
+        ORG 2000h
+   ARR1 DW  01H,02H,04H,04H,08H,0CH
 
-DATA ENDS 
+DATA ENDS
 
 CODE SEGMENT
-    ASSUME CS:CODE ,DS:DATA
-START:
-   MOV AX,DATA
-   MOV DS,AX
-   XOR AX,AX
-   LEA SI,ARR1
-   MOV BL,04H
-   MOV CX,6
-   XOR DX,DX
+          ASSUME CS:CODE ,DS:DATA
+   START: 
+          MOV    AX,DATA
+          MOV    DS,AX
+          XOR    AX,AX
+          LEA    SI,ARR1
+          MOV    BL,04H
+          MOV    CX,06H
+          XOR    DX,DX
 
-LOOP1:
-   MOV AX,[SI]
-   DIV BL
-   CMP AH,00H
-   JNZ LABEL1
-   INC SI
-   INC SI
-   INC DX
-   LOOP LOOP1
+   LOOP1: 
+          MOV    AX,[SI]
+          DIV    BL
+          CMP    AH,00H
+          JNZ    LABEL1
+          INC    SI
+          INC    SI
+          INC    DL
+          LOOP   LOOP1
+          CMP    CX,00H
+          JZ     LABEL2
    
    
 
 
-LABEL1:
-   INC SI
-   INC SI
-   LOOP LOOP1
+   LABEL1:
+          INC    SI
+          INC    SI
+          LOOP   LOOP1
 
-
-HLT
-CODE ENDS 
+   LABEL2:
+          HLT
+CODE ENDS
 END START
